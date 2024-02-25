514250cb74cdc6bb1f3ea5cd216b8d38a12f0bd6
maashha <72510050+maashha@users.noreply.github.com>
2024-02-25 14:26:02 +0200

    Update plagiarism_2.py

--- plagiarism_2.py
+++ plagiarism_2.py
@@ -10,14 +10,11 @@ import zipfile
 np.random.seed(0)
 sns.set_theme()
 tokens_list = []
-filename = 'Архив_2.zip'
+filename = 'DIR.zip'
 with zipfile.ZipFile(filename) as zip:
     namelist = zip.namelist()
 number_files = len(namelist)
-# проводим токенизацию кода
-# для каждого слова кода определяем является он строкой, числом и т.д.
-# каждый отдельный тип слов обозначаем своим символом 
-# после анализа всего кода получаем строку, состоящую из этим символов
+
 for files in range(0, number_files, 2):
     with zipfile.ZipFile('Архив_2.zip') as thezip:
         with tokenize.open(namelist[files]) as f:
@@ -45,7 +42,7 @@ for files in range(0, number_files, 2):
             tokens_list.append(token1)
 
 
-# сравниваем каждые две строки по алгоритму шинглов
+
 def shingle(word):
     shingleLen = 4
     shingles = []
@@ -62,7 +59,7 @@ def compare(shingles1, shingles2):
         if shingles1[shingle] in shingles2:
             same = same + 1
     return same/(len(shingles1) + len(shingles2)-same)*100
-# создаем массив, в котором будут находиться итоговые значения попарных сранений
+
 resoults = []
 lines = []
 for number_name in range(number_files//2):
@@ -72,7 +69,7 @@ for number_name in range(number_files//2):
     for name_number in range(number_files//2):
         lines.append(100)
 resoults.append(lines)
-# выводим результаты для каждых двух сравнимаемых программ
+
 for first_file in range(number_files//2):
     for second_file in range(first_file+1, number_files//2):
         if compare(list(shingle(tokens_list[first_file])),
@@ -80,11 +77,10 @@ for first_file in range(number_files//2):
             finalPercent = compare(list(shingle(tokens_list[first_file])),
                         list(shingle(tokens_list[second_file])))
             print(namelist[first_file*2], namelist[second_file*2], finalPercent)
-        # добавляем значения процента схожести в массивы с результатами
+        
         resoults[first_file][second_file] = finalPercent
         resoults[second_file][first_file] = finalPercent
-# делаем из массива с результатами DataFrame
-# по его данным строим heatmap
+
 DataFrame_resoults = pd.DataFrame(resoults)
 sns.heatmap(DataFrame_resoults, cmap="ocean",  vmin=0, vmax=100)
 plt.show()
